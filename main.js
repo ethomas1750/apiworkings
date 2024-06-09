2f9d58f499eccbb15a8ad75fcefd62b3bb6a9a74
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-08 23:22:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+const dogUrl = 'https://dog.ceo/api/breeds/image/random'
+const weatherButton = document.querySelector('#weatherButton')
 
+
+  dogButton.addEventListener('click', (event)=>{
+    console.log('dog button pushed')
+    event.preventDefault()
+  fetch(dogUrl)
+  .then((response)=>{
+    console.log('response received')
+    return response.json()
+  })
+  .then((object)=>{
+    console.log('response processed')
+    console.log(object.message)
+    dogImage.src = object.message
+    console.log(object)
+  })
+
+})
+
+weatherButton.addEventListener('click', ()=>{
+const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latBox.value}&longitude=${longBox.value}&current=temperature_2m&temperature_unit=fahrenheit&current=weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=mph`
+console.log('weather button pushed')
+fetch(weatherUrl)
+.then((response)=>{
+    console.log('request received')
+    // console.log(response.json())
+    return response.json()
+})
+.then((object)=>{
+    console.log('request processed')
+    console.log(object)
